8627445df29230252ca161c5cbfcc19baa13bf4b
Joan Gramuntell <jgramuntell@alfatecsistemas.es>
2018-03-07 11:02:36 +0000

    Se modifica tags.html y controllers para que aparezca el total de los tags
    git-svn-id: http://scm.alfatecsistemas.es/svn/opendata/barcelona/fuentes/ckan/trunk/default/src/ckanext-odatabcn@1193 77a2d1f1-ac09-48c7-bd20-9104370a2aaa

--- ckanext/odatabcn/controllers.py
+++ ckanext/odatabcn/controllers.py
@@ -220,7 +220,11 @@ class CSVController(t.BaseController):
 	def view_tags_html(self):
 			
 		#Obtenemos los tags
-		sql = '''SELECT DISTINCT name FROM tag ORDER BY name;'''
+		sql = '''SELECT T.name as name_tag, COUNT(*) as total_tag FROM tag T
+					INNER JOIN package_tag PT ON PT.tag_id = T.id
+					WHERE PT.state LIKE 'active'
+					GROUP BY T.name
+					ORDER BY T.name;'''
 		results = model.Session.execute(sql)
 		
 		t.response.headers['Content-Type'] = 'text/html; charset=utf-8'
